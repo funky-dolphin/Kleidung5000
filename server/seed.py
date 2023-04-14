d1c7a493c46cbecd8dca9eb5a9d06a7d50e78095
Brett Polacek <brettpolacek@gmail.com>
2023-04-14 16:00:08 -0400

    models, seed

--- server/seed.py
+++ server/seed.py
@@ -3,6 +3,103 @@ from faker import Faker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app import app
-from models import db 
+from models import db, User, Item, Type, SubType, Size, Brand, Transaction, Message
 
-# with app.app_context():
+
+with app.app_context():
+
+# This will delete any existing rows
+# so you can run the seed file multiple times without having duplicate entries in your database
+    print("Deleting data...")
+    User.query.delete()
+    Item.query.delete()
+    Type.query.delete()
+    SubType.query.delete()
+    Size.query.delete()
+    Brand.query.delete()
+    Transaction.query.delete()
+    Message.query.delete()
+
+    print("Creating users...")
+    nick=User(name="Nick", email_address ="nick@kleidung5000.com", paypal_address = "nick@kleidung5000.com", zipcode = 10003)
+    finn=User(name="Finn", email_address ="finn@kleidung5000.com", paypal_address = "finn@kleidung5000.com", zipcode = 10004)
+    brett=User(name="Brett", email_address ="brett@kleidung5000.com", paypal_address = "brett@kleidung5000.com", zipcode = 11237)
+    users = [nick, finn, brett]
+
+    print("Creating items...")
+    i1=Item(condition=8, image = " ", color="black", name="myshirtttt", price = 100.99, for_sale=True, owner_id=1, type_id=1, subtype_id=1, size_id=3, brand_id=2)
+    i2=Item(condition=3, image = " ", color="grey", name="thong", price = 444.99, for_sale=True, owner_id=2, type_id=1, subtype_id=2, size_id=4, brand_id=3)
+    items=[i1,i2]
+
+    print("Creating types...")
+    outerwear = Type(type = "Outerwear")
+    tops = Type(type = "Tops")
+    bottoms = Type(type = "Bottoms")
+    footwear = Type(type = "Footwear")
+    accessories = Type(type="Accessories")
+    types = [outerwear, tops, bottoms, footwear, accessories]
+
+    print("Creating subtypes...")
+    jackets = SubType(subtype = "Jackets", type_id=1)
+    coats = SubType(subtype = "Coats", type_id = 1)
+    trenchcoats = SubType(subtype = "Trench Coats", type_id = 1)
+    tailoring = SubType(subtype = "Tailoring", type_id=1)
+    fur =  SubType(subtype = "Fur", type_id=1)
+    down = SubType(subtype = "Down", type_id=1)
+
+    knits = SubType(subtype = "Knits", type_id = 2)
+    sweatshirts = SubType(subtype = "Sweatshirts", type_id = 2)
+    tshirts = SubType(subtype = "T-Shirts", type_id = 2)
+    tanks = SubType(subtype = "Tanks", type_id = 2)
+    buttonups = SubType(subtype = "Button Ups", type_id = 2)
+
+    subtypes = [jackets, coats, trenchcoats, tailoring, fur, down, knits, sweatshirts, tshirts, tanks, buttonups]
+
+
+
+    print("Creating sizes...")
+    xs = Size(size = "XS")
+    s = Size(size = "S")
+    m = Size(size = "M")
+    l = Size(size = "L")
+    xl = Size(size = "XL")
+    xxl = Size(size = "XXL")
+    sizes = [xs, s, m, l, xl, xxl]
+
+    print("Creating brands...")
+    cdg = Brand(brand = "Comme des Garcons")
+    acronym = Brand(brand = "Acronym")
+    gucci = Brand(brand = "Gucci")
+    hermes = Brand(brand = "Hermes")
+    kikokostadinov = Brand(brand = "Kiko Kostadinov")
+    prada = Brand(brand = "Prada")
+    brands = [cdg, acronym, gucci, hermes, kikokostadinov, prada]
+
+    print("Creating transactions...")
+    t1=Transaction(buyer_id = 1, seller_id=2, price=200, item_id=2)
+    t2=Transaction(buyer_id = 2, seller_id=1, price =100, item_id = 1)
+    t3=Transaction(buyer_id = 2, seller_id=3, price=400, item_id=4)
+    transactions = [t1, t2, t3]
+
+    print("Creating messages...")
+    m1=Message(user_1=1, user_2=2, message="yo")
+    m2=Message(user_1=2, user_2=1, message="wassup")
+    m3=Message(user_1=1, user_2=3, message="brett is a dick")
+    messages=[m1,m2,m3]
+
+    db.session.add_all(users)
+    db.session.add_all(items)
+    db.session.add_all(types)
+    db.session.add_all(subtypes)
+    db.session.add_all(sizes)
+    db.session.add_all(brands)
+    db.session.add_all(transactions)
+    db.session.add_all(messages)
+    db.session.commit()
+
+    print("Seeding done!")
+    print(outerwear.subtypes[1].subtype)
+
+    
+
+    
