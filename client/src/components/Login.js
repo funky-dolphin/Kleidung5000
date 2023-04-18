76e34906f1d82e85e2a63f17f69cd37eb2213058
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-04-18 14:07:52 -0400

    navbar fix

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,34 +1,33 @@
 import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
 
 function Login() {
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col xs={12} md={6}>
-          <h2 style={{ color: "whitesmoke" }} className="text-center">
-            Welcome Back To KLEIDUNG5000
-          </h2>
-          <h3 style={{ color: "whitesmoke" }} className="text-center">
-            Please Login
-          </h3>
-          <Form>
-            <Form.Group controlId="formBasicEmail" className="mt-3">
-              <Form.Label style={{ color: "whitesmoke" }}>
-                Email address
-              </Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+          <Card className="mt-5">
+            <Card.Body>
+              <Card.Title className="text-center mb-4">
+                Welcome Back To KLEIDUNG5000
+              </Card.Title>
+              <Form>
+                <Form.Group controlId="formBasicEmail" className="mt-3">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
 
-            <Form.Group controlId="formBasicPassword" className="mt-3">
-              <Form.Label style={{ color: "whitesmoke" }}>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
+                <Form.Group controlId="formBasicPassword" className="mt-3">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
 
-            <Button variant="primary" type="submit" className="w-100 mt-3">
-              Submit
-            </Button>
-          </Form>
+                <Button variant="primary" type="submit" className="w-100 mt-3">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Navbar,
   Nav,
@@ -16,6 +16,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
+  const [showOuterwear, setShowOuterwear] = useState(false);
+  const [showTops, setShowTops] = useState(false);
+  const [showBottoms, setShowBottoms] = useState(false);
+  const [showFootwear, setShowFootwear] = useState(false);
+  const [showAccessories, setShowAccessories] = useState(false);
+
   const navigate = useNavigate();
   // click functions
   const handleLoginClick = () => {
@@ -47,7 +53,14 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <NavDropdown title="Outerwear" id="basic-nav-dropdown">
+            <NavDropdown
+              title="Outerwear"
+              id="basic-nav-dropdown"
+              show={showOuterwear}
+              onMouseEnter={() => setShowOuterwear(true)}
+              onMouseLeave={() => setShowOuterwear(false)}
+              className="hover-dropdown"
+            >
               <NavDropdown.Item href="#action/3.1">Jackets</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Coats</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
@@ -57,7 +70,14 @@ function Navigation() {
               <NavDropdown.Item href="#action/3.5">Fur</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.6">Down</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Tops" id="basic-nav-dropdown">
+            <NavDropdown
+              title="Tops"
+              show={showTops}
+              onMouseEnter={() => setShowTops(true)}
+              onMouseLeave={() => setShowTops(false)}
+              id="basic-nav-dropdown"
+              className="hover-dropdown"
+            >
               <NavDropdown.Item href="#action/3.1">Knits</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Sweatshirts
@@ -66,17 +86,38 @@ function Navigation() {
               <NavDropdown.Item href="#action/3.4">Tanks</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.5">ButtonUps</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Bottoms" id="basic-nav-dropdown">
+            <NavDropdown
+              title="Bottoms"
+              id="basic-nav-dropdown"
+              show={showBottoms}
+              onMouseEnter={() => setShowBottoms(true)}
+              onMouseLeave={() => setShowBottoms(false)}
+              className="hover-dropdown"
+            >
               <NavDropdown.Item href="#action/3.1">Jeans</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Trousers</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Shorts</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Footwear" id="basic-nav-dropdown">
+            <NavDropdown
+              title="Footwear"
+              id="basic-nav-dropdown"
+              show={showFootwear}
+              onMouseEnter={() => setShowFootwear(true)}
+              onMouseLeave={() => setShowFootwear(false)}
+              className="hover-dropdown"
+            >
               <NavDropdown.Item href="#action/3.1">Sneakers</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Boots</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Sandals</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Accessories" id="basic-nav-dropdown">
+            <NavDropdown
+              title="Accessories"
+              id="basic-nav-dropdown"
+              show={showAccessories}
+              onMouseEnter={() => setShowAccessories(true)}
+              onMouseLeave={() => setShowAccessories(false)}
+              className="hover-dropdown"
+            >
               <NavDropdown.Item href="#action/3.1">Bags</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Jewelry</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Glasses</NavDropdown.Item>
